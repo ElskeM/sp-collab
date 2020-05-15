216ae3dc0c118242d32d6a9cfc002bf4922f9a93
Bollpiska <bollpiska@hotmail.com>
2020-05-15 15:35:36 +0200

    webservice

--- src/service/OlfWebserviceImplementation.java
+++ src/service/OlfWebserviceImplementation.java
@@ -6,6 +6,8 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jws.WebService;
 
+import dao.ArticleNotFoundException;
+import dao.CustomerNotFoundException;
 import dao.DataAccess;
 import domain.Article;
 import domain.Customer;
@@ -20,21 +22,21 @@ public class OlfWebserviceImplementation{
 	
 	
 	public List<CustomerOrder> findAllOrders() {
-		return service.findAllOrders();
+		return service.getAllOrders();
 	}
 
 
 	public List<Customer> findAllCustomer() {
 
 		// TODO Auto-generated method stub
-		return service.findAllCustomer();
+		return service.getAllCustomer();
 	}
 
 
 	public List<Article> findAllArticle() {
 
 		// TODO s-generated method stub
-		return service.findAllArticle();
+		return service.getAllArticle();
 	}
 
 	public Article register(Article article) {
@@ -45,7 +47,7 @@ public class OlfWebserviceImplementation{
 	}
 
 	
-	public void register(CustomerOrder customerOrder) {
+	public void register(CustomerOrder customerOrder) throws ArticleNotFoundException, CustomerNotFoundException {
 
 		// TODO Auto-generated method stub
 		service.register(customerOrder);		
