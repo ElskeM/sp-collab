0f8f5a7516536edd31cb7188819605f89adb1789
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-05-26 15:00:32 +0200

    handle OutOfStockException

--- src/rest/CustomerOrderResource.java
+++ src/rest/CustomerOrderResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -21,6 +22,7 @@ import javax.ws.rs.core.UriInfo;
 import dao.ArticleNotFoundException;
 import dao.CustomerNotFoundException;
 import dao.OrderNotFoundException;
+import dao.OutOfStockException;
 import domain.CustomerOrder;
 import service.OlfService;
 import service.ServiceUnavailableException;
@@ -76,6 +78,9 @@ public class CustomerOrderResource {
 			return Response.status(404).build();
 		} catch (ServiceUnavailableException e) {
 			return Response.status(504).build();
+		} catch (OutOfStockException e1) {
+			Entity errorEntity = Entity.entity(e1.getMessage(), "application/JSON");
+			return Response.ok(errorEntity).build();
 		}
 	}
 
