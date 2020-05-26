f273c0a63864d1717826bf62d6b5ce99756120ee
Bollpiska <bollpiska@hotmail.com>
2020-05-26 20:51:42 +0200

    ändrade i getAllArticles så att den koms åt ifrån webbem samt skapade en CustomerWebResource-klass

---
+++ src/rest/CustomerWebResource.java
@@ -0,0 +1,58 @@
+package rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import domain.Customer;
+import service.OlfService;
+import service.ServiceUnavailableException;
+
+@Stateless
+@Path("/customersweb")
+public class CustomerWebResource {
+	
+	@Inject
+	private OlfService service;
+	
+	@Context
+	private UriInfo uriInfo;
+	
+	@POST
+	@Produces({ "application/JSON" })
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response registerCustomer(@FormParam("firstname") String firstName, 
+									 @FormParam("lastname") String lastName,
+									 @FormParam("address") String address,
+									 @FormParam("zipcode") String zipCode,
+									 @FormParam("city") String city
+									 ) {
+		
+		Customer customer = new Customer(firstName, lastName, address, zipCode, city, 0);
+		
+		try {
+			service.register(customer);
+		} catch (ServiceUnavailableException e1) {
+			return Response.status(500).build();
+		}
+		URI uri = null;
+			try {
+				uri = new URI(uriInfo.getAbsolutePath() + "/" + customer.getCustomerNr());
+			} catch (URISyntaxException e) {}
+	
+		return Response.created(uri).build();
+
+	}
+
+}
--- src/service/ReceiptSendingService.java
+++ src/service/ReceiptSendingService.java
@@ -5,7 +5,7 @@ public class ReceiptSendingService {
 	public static void sendReciept(String firstName, String lastName, String adress, String zipcode, String city)
 	throws ServiceUnavailableException { 
 		double rand = Math.random();
-		if(rand < 0.5) {
+		if(rand < 0) {
 			throw new ServiceUnavailableException();
 		}
 		
