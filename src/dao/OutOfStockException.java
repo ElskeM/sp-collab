d04168c58a788ca213610bb39485dd4691449df6
Simon Hagelin <simon_hagelin@hotmail.com>
2020-05-26 14:33:29 +0200

    Added string constructor

--- src/dao/OutOfStockException.java
+++ src/dao/OutOfStockException.java
@@ -2,7 +2,14 @@ package dao;
 
 import javax.ejb.ApplicationException;
 
+/**Exception
+ * @author simon
+ *
+ */
 @ApplicationException(rollback=true)
 public class OutOfStockException extends Exception {
 
+	public OutOfStockException(String s) {
+		super(s);
+	}
 }
